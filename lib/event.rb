da1b2799c6233dd9c8f123d104910eb581c1e871
Judith Pillado <judith.pillado@gmail.com>
2020-04-15 12:52:21 -0500

    Add food truck array

--- lib/event.rb
+++ lib/event.rb
@@ -1,9 +1,23 @@
 class Event
-  attr_reader :name, :food_trucks 
+  attr_reader :name, :food_trucks
 
   def initialize(name)
     @name = name
     @food_trucks = []
   end
+
+  def add_food_truck(food_truck)
+    @food_trucks << food_truck
+  end
+
+  # def potential_revenue
+  #   @item.price.sum * amount
+  # end
+
+
+  # def food_trucks_that_sell(item)
+  # end
+
+
 
 end
