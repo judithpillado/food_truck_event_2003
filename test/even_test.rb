0c30ab4826af69347176fe38e5f5e32d53240c51
Judith Pillado <judith.pillado@gmail.com>
2020-04-15 12:38:10 -0500

    Add attributes

--- test/even_test.rb
+++ test/even_test.rb
@@ -6,4 +6,17 @@ require './lib/event'
 
 class EventTest < Minitest::Test
 
+  def setup
+    @event = Event.new("South Pearl Street Farmers Market")
+  end
+
+  def test_it_exists
+    assert_instance_of Event, @event
+  end
+
+  def test_it_has_attributes
+    assert_equal "South Pearl Street Farmers Market", @event.name
+    assert_equal [], @event.food_trucks
+  end
+
 end
