82c3f9cb2eb44c6e53c972a0f261f6b2a1e5908a
Judith Pillado <judith.pillado@gmail.com>
2020-04-15 11:30:01 -0500

    Add test it has attributes

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -13,4 +13,8 @@ class FoodTruckTest < Minitest::Test
     assert_instance_of FoodTruck, @food_truck
   end
 
+  def test_it_has_attributes
+    assert_equal "Rocky Mountain Pies", @food_truck.name
+  end 
+
 end
